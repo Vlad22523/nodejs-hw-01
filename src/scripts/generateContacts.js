b03ec9583831ddfe4790053a9c6d0c3adfba9563
Vlad Melnyk <vlad22523@gmail.com>
2024-09-19 06:40:57 +0300

    hw-01

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const newArr = [];
+    const contacts = await readContacts();
+    for (let i = 0; i < number; i++) {
+      newArr.push(createFakeContact());
+    }
+    const updatedContacts = [...contacts, ...newArr];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 generateContacts(5);
